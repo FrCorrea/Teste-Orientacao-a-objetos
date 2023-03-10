857fa04c73b3d84591a28f521dfd80caeb1d7af6
Francisco Correa <franciscocor.neto@gmail.com>
2023-03-10 20:20:17 -0300

    funções completas

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,23 @@ public class Main {
     public static void main(String[] args) {
         Service service = new Service();
         Repositorie repositorie = new Repositorie();
-
-
         List<Seller> sellers= repositorie.getSellers();
-
         List<Employee> employees = repositorie.getEmployees();
+        List<Employee> beneficiaries = repositorie.getBeneficiaries();
 
-        System.out.printf(String.valueOf(service.sumSalary(employees,2,2021)));
-        Seller topSeller = service.topSeller(sellers,2,2022);
+        System.out.println("1 - " + String.valueOf(service.sumSalary(employees,2,2022)));
+        System.out.println("2 - " + service.totalMonthSalary(employees,2,2022));
+        System.out.println("3 - " + service.totalBeneficiesMonth(beneficiaries,2,2022));
+
+        Employee topEmployee = service.highestPaidEmployee(employees,2,2022);
+        System.out.println("4 - " + topEmployee.name);
+
+        System.out.println("5 - " + service.highestBeneficiaryEmployee(beneficiaries,2,2022));
+
+        Seller topSeller = service.topSeller(sellers,12,2021);
+        System.out.println("6 - " + topSeller.name);
 
 
-        System.out.printf("\n" + topSeller.name);
 
 
     }
